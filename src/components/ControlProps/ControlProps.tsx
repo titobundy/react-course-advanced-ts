3222c1a25654f7c150984cc1e44a045aa315e242
Hector Diaz <hdiaz@nectia.com>
2025-03-10 15:44:27 -0300

    feat(clase-3): add Toggle component with control props

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,40 @@
-export const ParentComponent = () => {
+import { useState } from 'react';
+
+type ToggleProps = {
+  isToggled?: boolean;
+  onToggle: (value: boolean) => void;
+};
+
+export const Toggle: React.FC<ToggleProps> = ({
+  isToggled = false,
+  onToggle,
+}) => {
+  const [internalToggled, setInternalToggle] = useState(isToggled);
+
+  const toggle = () => {
+    const newValue = !internalToggled;
+    setInternalToggle(newValue);
+
+    if (onToggle) {
+      console.log("onToggle", newValue);
+      onToggle(newValue);
+    }
+  };
+
   return (
     <div>
+      <button onClick={toggle}>Toggle {internalToggled ? "ON 🥳" : "OFF 🤒"}</button>
+    </div>
+  );
+};
+
+export const ParentComponent = () => {
+  const [toggleState, setToggleState] = useState(false);
+
+  return (
+    <div>
+      <p>Toggle State: {toggleState ? "ON" : "OFF"}</p>
+      <Toggle isToggled={toggleState} onToggle={setToggleState} />
     </div>
   );
 };
