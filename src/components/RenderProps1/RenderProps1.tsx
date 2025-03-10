5970b1aa3c2d1c54d7323ad6873bcf14d45f0c96
Hector Diaz <hdiaz@nectia.com>
2025-03-10 15:32:39 -0300

    feat(clase-2): add ChildComponent for render props pattern

--- src/components/RenderProps1/RenderProps1.tsx
+++ src/components/RenderProps1/RenderProps1.tsx
@@ -1,6 +1,16 @@
+import { JSX } from 'react';
+
+type ChildComponentProps = {
+  render: (name: string) => JSX.Element;
+};
+
+const ChildComponent: React.FC<ChildComponentProps> = ({ render }) => {
+  const name = 'Teffcode';
+  return <div>{render(name)}</div>;
+};
+
 export const ParentComponent = () => {
   return (
-    <>
-    </>
+    <ChildComponent render={(name) => <p>Hello, {name} !</p>} />
   );
 };
