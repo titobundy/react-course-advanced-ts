ee53d51a8d8201c8dc79406fb13b601ac01756cc
Hector Diaz <hdiaz@nectia.com>
2025-03-11 12:48:25 -0300

    feat(clase-7): Container-Presenter Pattern

--- src/components/ContainerPresenter/DataContainer.tsx
+++ src/components/ContainerPresenter/DataContainer.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
+import { DataPresenter, DataItem } from './DataPresenter';
+
 export const DataContainer = () => {
+  const [data, setData] = useState<DataItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+        const response = await fetch('/data/data.json');
+        if (!response.ok) {
+          throw new Error('Error fetching data');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(`Error fetching data: ${error}`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {data.length > 0 && <DataPresenter data={data}></DataPresenter>}
     </div>
   );
 };
--- src/components/ContainerPresenter/DataPresenter.tsx
+++ src/components/ContainerPresenter/DataPresenter.tsx
@@ -1,6 +1,25 @@
-export const DataPresenter = () => {
+import React from 'react';
+
+export type DataItem = {
+  id: number;
+  name: string;
+  description: string;
+  image: string;
+};
+
+type DataPresenterProps = {
+  data: DataItem[];
+};
+
+export const DataPresenter: React.FC<DataPresenterProps> = ({ data }) => {
   return (
     <>
+      {data.map((item) => (
+        <React.Fragment key={item.id}>
+          <img src={item.image} alt={item.name} />
+          <em>{item.description}</em>
+        </React.Fragment>
+      ))}
     </>
   );
 };
