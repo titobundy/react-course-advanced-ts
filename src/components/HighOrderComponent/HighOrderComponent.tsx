d1d01377d6c229f758b5741028b0361ee0350a1d
Hector Diaz <hdiaz@nectia.com>
2025-03-11 11:55:57 -0300

    feat(clase-5): add withLoading High-Order Components

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,25 @@
+function withLoading<T extends object>(Component: React.ComponentType<T>) {
+
+  return (props: T & { isLoading: boolean }) => {
+    const { isLoading, ...rest } = props
+    return isLoading ? <div>Loading...</div> : <Component {...rest as T} />;
+  }
+}
+
+type UserProps = {
+  name: string;
+}
+const UserComponent: React.FC<UserProps> = ({ name }) => {
+  return <p>Hello, {name}!</p>
+};
+
+const UserWithLoading = withLoading(UserComponent);
+
 export const ParentComponent = () => {
   return (
     <div>
+      <UserWithLoading isLoading={true} name="Teffcode" />
+      <UserWithLoading isLoading={false} name="Teffcode" />
     </div>
   );
 };
