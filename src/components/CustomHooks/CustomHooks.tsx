57bd452934dcb61441b70369348596b41017e3fe
Hector Diaz <hdiaz@nectia.com>
2025-03-11 12:11:30 -0300

    feat(CustomHooks): implement useLoading with Custom Hooks

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,39 @@
-const UserComponent = () => {
+
+import React, { useEffect, useState } from "react";
+
+function useLoading(initialLoading: boolean = false) {
+  const [isLoading, setIsLoading] = useState(initialLoading);
+  
+  return {  isLoading, setIsLoading };
+}
+type UserProps = {
+  name: string;
+}
+
+const UserComponent: React.FC<UserProps> = ({name}) => {
+  const { isLoading, setIsLoading } = useLoading(false);
+
+  useEffect(() => {
+    if(isLoading) {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }
+  }
+  , [isLoading, setIsLoading]);
+
+  if(isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
+      <p>Hello, {name}!</p>
+      <button onClick={() => setIsLoading(true)}>Simulate Loading</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name='Teffcode' />;
 };
