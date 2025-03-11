a486bd25d701059853d10fafbada18d317c117ac
Hector Diaz <hdiaz@nectia.com>
2025-03-11 14:57:45 -0300

    feat(clase-8): Controlled and Uncontrolled components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState('');
+
   return (
     <div>
+      <input
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        placeholder='Ingresa el código del cupón'
+      />
+      <p>
+        Cupón de descuento: <b>{value}</b>
+      </p>
     </div>
   );
 };
--- src/components/UncontrolledComponent/UncontrolledComponent.tsx
+++ src/components/UncontrolledComponent/UncontrolledComponent.tsx
@@ -1,6 +1,17 @@
+import { useRef } from 'react';
+
 export const UncontrolledInput = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = () => {
+    if(!inputRef.current?.value) return;
+
+    alert(`Producto añadido: ${inputRef.current.value} ✅`);
+  };
+
   return (
     <div>
+      <input ref={inputRef} placeholder='Nombre del producto. Ej: Manzana' />
+      <button onClick={handleSubmit}>Añadir al carrito</button>
     </div>
   );
 };
